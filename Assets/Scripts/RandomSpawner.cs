fbf731b838b777005b6d131c51914b337b4ecf04
NikitaBagulov <117837048+NikitaBagulov@users.noreply.github.com>
2023-05-04 13:02:20 +0800

    Upgrade RandomSpawnerEnemy

--- Assets/Scripts/RandomSpawner.cs
+++ Assets/Scripts/RandomSpawner.cs
@@ -9,6 +9,9 @@ public class RandomSpawner : MonoBehaviour
     public Transform spawnPoint;
     public GameObject[] enemyPrefabs;
     public Vector2 volume;
+    private float x = Random.Range(0f, 100f);
+    private float y = Random.Range(0f, 100f);
+    
     void Start()
     {
         StartCoroutine("SpawnEnemy");
@@ -24,10 +27,15 @@ public class RandomSpawner : MonoBehaviour
     IEnumerator SpawnEnemy()
     {
         while(true){
-            enemyCount = 20;
+            enemyCount = 5;
             Debug.Log("Spawn");
             for (int i = 0; i< enemyCount; i++){
-                Vector2 pos = new Vector2(Random.Range(spawnPoint.position.x - volume.x, spawnPoint.position.x + volume.x), Random.Range(spawnPoint.position.y - volume.y, spawnPoint.position.y+volume.y));
+                float rand_x = Random.Range(spawnPoint.position.x - volume.x, spawnPoint.position.x + volume.x);
+                float rand_y = Random.Range(spawnPoint.position.y - volume.y, spawnPoint.position.y + volume.y);
+                rand_x  = rand_x > spawnPoint.position.x ? rand_x+15 : rand_x-15;
+                rand_y  = rand_y > spawnPoint.position.y ? rand_y+8 : rand_y-8;
+                
+                Vector2 pos = new Vector2(rand_x, rand_y);
                 GameObject obj = Instantiate(enemyPrefabs[0], pos, Quaternion.identity);
                 
             } 
