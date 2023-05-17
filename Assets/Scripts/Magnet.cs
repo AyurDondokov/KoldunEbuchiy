dc94127f0c0b20f34d90e03392b6ad7b33be7982
Yuri-ga1 <regota09@gmail.com>
2023-05-17 16:39:40 +0800

    m->M

--- Assets/Scripts/Magnet.cs
+++ Assets/Scripts/Magnet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class magnet : MonoBehaviour
+public class Magnet : MonoBehaviour
 {
     [SerializeField] float[] size;
 
