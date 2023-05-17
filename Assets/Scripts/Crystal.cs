3daae8212967b2fdc66c19c8f670b0976badb77a
Yuri-ga1 <regota09@gmail.com>
2023-05-17 20:05:13 +0800

    Update Crystal.cs

--- Assets/Scripts/Crystal.cs
+++ Assets/Scripts/Crystal.cs
@@ -7,6 +7,6 @@ public class Crystal : MonoBehaviour
     public int xp;
 
     private void Start() {
-        transform.rotation = Quaternion.Euler(0,0,Random.Range(0,181));    
+        transform.rotation = Quaternion.Euler(0,0,Random.Range(0,360));    
     }
 }
